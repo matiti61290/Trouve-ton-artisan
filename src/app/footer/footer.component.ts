f49ecda7bc3429daf78407e19e161e208895afd0
Mathieu <mathieu.barbey18@gmail.com>
2024-08-26 16:34:21 +0200

    add social icon

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SocialComponent } from '../component/social/social.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    SocialComponent
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
