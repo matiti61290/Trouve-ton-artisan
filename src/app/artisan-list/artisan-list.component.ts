7262dfebdd20c61d2dc067685770206113dceca9
Mathieu <mathieu.barbey18@gmail.com>
2024-09-16 15:23:11 +0200

    style added

--- src/app/artisan-list/artisan-list.component.ts
+++ src/app/artisan-list/artisan-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ArtisanCardComponent } from '../component/artisan-card/artisan-card.component';
 
 @Component({
   selector: 'app-artisan-list',
   standalone: true,
-  imports: [],
+  imports: [
+    ArtisanCardComponent  
+  ],
   templateUrl: './artisan-list.component.html',
   styleUrl: './artisan-list.component.scss'
 })
